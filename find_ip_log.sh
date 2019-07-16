1a2a430ff568b0516dc4c4c91a97ff4ffb8de5f9
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-07-16 15:35:01 +0100

    Update find_ip_log.sh

--- find_ip_log.sh
+++ find_ip_log.sh
@@ -2,7 +2,8 @@
 
 FILE=appache.log;
  for ip in `cat $FILE |cut -d ' ' -f 1 |sort |uniq`;
- do { COUNT=`grep ^$ip $FILE |wc -l`;
+ do { 
+ 	 COUNT=`grep ^$ip $FILE |wc -l`;
      if [[ "$COUNT" -lt "500" ]]; 
      	then echo "$COUNT:   $ip";
      fi 
