4ba7afca7038d757ebdfe16c0a50fe2cd2f41d90
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-07-16 10:31:45 +0100

    Create 02_conditon.sh

---
+++ 02_conditon.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# GNU bash, version 4.3.46
+if [$(whoami) != 'root']; then
+   echo "Must be root to run"
+fi
+
+if[-z $1]; then
+  echo "Usage: $0 </path/to/httpd.conf>"
+  exit 1
+fi
+
+httpd_conf_new=$1
+httpd_con_path="/usr/local/apache/conf"
+login=htuser
+
+farm_hosts=(web03 web04 web05 web06 web07)
+
+for i in ${farm_hosts[@]}; do
+  su $login -c "scp $httpd_conf_new ${i}:${httpd_conf_path}"
+  su $login -c "scp  sudo /usr/local/apache/bin/apachectl graceful"
+done
+exit 0
