35d73980b12c8245cf8a645646b0ea0d4457b7db
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-07-16 15:34:21 +0100

    Create find_ip_log.sh

---
+++ find_ip_log.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FILE=appache.log;
+ for ip in `cat $FILE |cut -d ' ' -f 1 |sort |uniq`;
+ do { COUNT=`grep ^$ip $FILE |wc -l`;
+     if [[ "$COUNT" -lt "500" ]]; 
+     	then echo "$COUNT:   $ip";
+     fi 
+   }; 
+done
