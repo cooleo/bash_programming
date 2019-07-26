54452b7758e1acb9e5af878cedc5a5aa401456d8
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-07-26 04:24:19 +0100

    Create 001_admin.sh

---
+++ linux/001_admin.sh
@@ -0,0 +1,13 @@
+#Check os version in Linux
+cat /etc/os-release
+lsb_release -a
+hostnamectl
+#linux kernel version
+uname -r
+#Another option is to type the following command:
+
+cat /proc/version
+#You can also view the manual page on uname using the following command:
+ man hostnamectl
+ man uname
+ man cat
